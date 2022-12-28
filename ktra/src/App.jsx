7824357d5af7ec661f586ca4c823e551f69b0509
thanhhtungg1209 <108406067+thanhhtungg1209@users.noreply.github.com>
2022-12-28 22:47:04 +0700

    ktra

---
+++ ktra/src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Cart from './component/cart/Cart';
+
+function App() {
+
+  return (
+    <>
+    <Cart/>
+    </>
+    
+  );
+}
+
+export default App;
